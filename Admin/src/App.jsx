4e46d6a932ac77d938769efb4fb8f2dfa84d88f6
UDITA SINGH <udita.1294@gmail.com>
2025-07-17 15:57:18 +0530

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://restaurant-app-backend-f271.onrender.com";
 
   return (
     <div>
