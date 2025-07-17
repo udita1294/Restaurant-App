2ef43c54ac2fd5e1920a1fd65db46d7866d8d59f
UDITA SINGH <udita.1294@gmail.com>
2025-07-17 15:50:58 +0530

    Update assets.js

--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://restaurant-app-backend-f271.onrender.com'
+export const url = "https://restaurant-app-backend-f271.onrender.com"
